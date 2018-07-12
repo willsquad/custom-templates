28161cebf9ad61fcb88c68783902dc787476c916
Ashil John <willsquad.aj@gmail.com>
2018-07-12 15:40:05 +0530

    Base setup

---
+++ template-1/header/header.php
@@ -0,0 +1,9 @@
+<style>
+    <?php include('files/css/header.css'); ?>
+</style>
+
+<div id="header" class="col-12">
+    <div class="top_bar">
+        test
+    </div>
+</div>
---
+++ template-1/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>
+            Template 1
+        </title>
+        <link rel="stylesheet" href="files/css/bootstrap.css">
+        <link rel="stylesheet" href="files/css/style.css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700|Open+Sans:300,400,600,700|Raleway:600|Roboto+Slab" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    </head>
+<body>
+    <!-- START OF MAIN CONTAINER FLUID -->
+    <div class="container-fluid">
+        <!-- START OF MAIN ROW -->
+        <div class="row">
+
+        <!-- Header -->
+        <?php include('header/header.php') ?>
+        <!-- Header -->
+            
+        </div>
+        <!-- END OF MAIN ROW -->
+    </div>
+    <!-- END OF MAIN CONTAINER FLUID -->
+</body>
+
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="files/js/main.js"></script>
+
+</html>
