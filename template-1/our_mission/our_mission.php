cac403c56303bac48d0d703b5e05fd7d46e9ed5a
Ashil John <willsquad.aj@gmail.com>
2018-07-16 15:23:05 +0530

    Our mission added

--- template-1/our_mission/our_mission.php
+++ template-1/our_mission/our_mission.php
@@ -1,14 +1,78 @@
 <style>
-    <?php include('files/css/get_quote.css'); ?>
+    <?php include('files/css/our_mission.css'); ?>
 </style>
 
-<div id="get_quote" class="col-12">
-    <!-- Get Quote -->
-    <div class="get_quote_container max_width d-flex align-items-center justify-content-between flex-wrap">
-        <div class="get_quote_lhs">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
-        <div class="get_quote_rhs">
-            <a>Get Quote</a>
+<div id="our_mission" class="col-12">
+    <!-- Our Mission -->
+    <div class="row our_mission_container max_width d-flex justify-content-between flex-wrap">
+        <div class="our_mission_lhs col-12 col-lg-4">
+            <div class="our_mission_statement_div">
+                <h3>Our Mission</h3>
+                <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipis civelit sed quia non qui dolorem ipsum quia olor sit amet, consectetu ipsum quia dolor sit amet, consectetur quia olor.</p>
+                <a href="#">Read More</a>
+            </div>
+        </div>
+        <div class="our_mission_rhs col-12 col-lg-8">
+            <div class="row">
+
+                <!-- Mission subsection -->
+                <div class="mission_sub_section col-12 col-md-6">
+                    <div class="row">
+                        <div class="icon col-3 col-sm-2 col-md-3">
+                            <img src="our_mission/files/images/delicate_team.png" alt="">
+                        </div>
+                        <div class="detail col-9 col-sm-10 col-md-9">
+                            <h4>Dedicated Team</h4>
+                            <p>Neque porro quisquam est, qui do lorem ipsum quia dolor sit ametig, consectetur adipis civelit sed.</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Mission subsection -->
+
+                <!-- Mission subsection -->
+                <div class="mission_sub_section col-12 col-md-6">
+                    <div class="row">
+                        <div class="icon col-3 col-sm-2 col-md-3">
+                            <img src="our_mission/files/images/delivery_time.png" alt="">
+                        </div>
+                        <div class="detail col-9 col-sm-10 col-md-9">
+                            <h4>On-Time Delivery</h4>
+                            <p>Neque porro quisquam est, qui do lorem ipsum quia dolor sit ametig, consectetur adipis civelit sed.</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Mission subsection -->
+
+                <!-- Mission subsection -->
+                <div class="mission_sub_section col-12 col-md-6">
+                    <div class="row">
+                        <div class="icon col-3 col-sm-2 col-md-3">
+                            <img src="our_mission/files/images/update_technology.png" alt="">
+                        </div>
+                        <div class="detail col-9 col-sm-10 col-md-9">
+                            <h4>Updated Techonlogy</h4>
+                            <p>Neque porro quisquam est, qui do lorem ipsum quia dolor sit ametig, consectetur adipis civelit sed.</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Mission subsection -->
+
+                <!-- Mission subsection -->
+                <div class="mission_sub_section col-12 col-md-6">
+                    <div class="row">
+                        <div class="icon col-3 col-sm-2 col-md-3">
+                            <img src="our_mission/files/images/quality_assurance.png" alt="">
+                        </div>
+                        <div class="detail col-9 col-sm-10 col-md-9">
+                            <h4>Quality Assurance</h4>
+                            <p>Neque porro quisquam est, qui do lorem ipsum quia dolor sit ametig, consectetur adipis civelit sed.</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Mission subsection -->
+
+            </div>
         </div>
     </div>
-    <!-- Get Quote -->
+<!-- Our Mission -->
 </div>
