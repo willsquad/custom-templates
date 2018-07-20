20c7b313c67aa562d4569001f9dc8be87c5eb29c
Ashil John <willsquad.aj@gmail.com>
2018-07-20 15:22:12 +0530

    Contact Us Band added

--- template-1/contact_us_band/contact_us_band.php
+++ template-1/contact_us_band/contact_us_band.php
@@ -3,5 +3,12 @@
 </style>
 
 <div id="contact_us_band" class="col-12">
-    
+    <!-- Get Quote -->
+    <div class="contact_us_band_container max_width d-flex align-items-center justify-content-between flex-wrap">
+        <div class="contact_us_band_lhs">Feel free to get in touch with us if you have any questions</div>
+        <div class="contact_us_band_rhs">
+            <a>Contact Us</a>
+        </div>
+    </div>
+    <!-- Get Quote -->
 </div>
