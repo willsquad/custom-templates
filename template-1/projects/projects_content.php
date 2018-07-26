92bd392468a3c69410450140ea2c6f00ffabe10b
Ashil John <willsquad.aj@gmail.com>
2018-07-26 15:36:28 +0530

    Projects page started

---
+++ template-1/projects/projects_content.php
@@ -0,0 +1,13 @@
+<style>
+    <?php include('files/css/projects.css'); ?>
+</style>
+
+<div id="projects" class="col-12">
+    <div class="row max_width">
+       Projects Content
+    </div>
+</div>
+
+<script>
+    <?php include('files/js/faq.js'); ?>
+</script>
