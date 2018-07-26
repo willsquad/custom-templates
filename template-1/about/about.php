e9cd26304e0036c814d7516b3ae8881e41d5a30f
Ashil John <willsquad.aj@gmail.com>
2018-07-26 08:55:12 +0530

    About page split into multiple included files

--- template-1/about/about.php
+++ template-1/about/about.php
@@ -5,95 +5,12 @@
 <!-- Subsection content -->
 <div id="about_page_container">
 
-    <!-- About page Layer 1 -->
-    <div id="about" >
-        <div class="row max_width">
-            <div class="about_lhs col-12 col-md-5 col-lg-4">
-                <img src="about/files/images/about_service_img.jpg" alt="">
-            </div>
-            <div class="about_rhs col-12 col-md-7 col-lg-8">
-                <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione.</h3>
-                <p class="summary">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque laudantium debitis facere vero sunt optio ex ipsum vel minus reiciendis aperiam maiores.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, eius accusantium ipsum aliquid placeat quam. Beatae hic tenetur laborum, consectetur voluptas modi quaerat quas harum quis voluptates</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, eius accusantium ipsum aliquid placeat quam. Beatae hic tenetur laborum, consectetur voluptas modi quaerat quas harum quis voluptates</p>
-                <br>
-                <a href="#" class="get_quote_button">Get Quote</a>
-            </div>
-        </div>
-    </div>
-    <!-- About page Layer 1 -->
+    <!-- About page intro -->
+    <?php include('about/about_intro.php'); ?>
+    <!-- About page intro -->
 
     <!-- Awards Section -->
-    <div id="awards_section">
-        <div class="row max_width">
-            
-        <div class="col-lg-4 col-12 awards_lhs">
-            <img src="about/files/images/about_award_img.jpg" alt="">
-        </div>
-            
-            <div class="col-lg-8 col-12 awards_rhs">
-                <div class="row">
-                    <!-- Mission subsection -->
-                    <div class="mission_sub_section col-12 col-md-6">
-                        <div class="row">
-                            <div class="icon col-3 col-sm-2 col-md-3">
-                                <img src="about/files/images/award_icon-1.png" alt="">
-                            </div>
-                            <div class="detail col-9 col-sm-10 col-md-9">
-                                <h4>Dedicated Team</h4>
-                                <p>Neque porro quisquam est, qui do lorem ipsum quia dolor sit ametig, consectetur adipis civelit sed.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Mission subsection -->
-
-                    <!-- Mission subsection -->
-                    <div class="mission_sub_section col-12 col-md-6">
-                        <div class="row">
-                            <div class="icon col-3 col-sm-2 col-md-3">
-                                <img src="about/files/images/engineer_icon.png" alt="">
-                            </div>
-                            <div class="detail col-9 col-sm-10 col-md-9">
-                                <h4>On-Time Delivery</h4>
-                                <p>Neque porro quisquam est, qui do lorem ipsum quia dolor sit ametig, consectetur adipis civelit sed.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Mission subsection -->
-
-                    <!-- Mission subsection -->
-                    <div class="mission_sub_section col-12 col-md-6">
-                        <div class="row">
-                            <div class="icon col-3 col-sm-2 col-md-3">
-                                <img src="about/files/images/modern_equip_icon.png" alt="">
-                            </div>
-                            <div class="detail col-9 col-sm-10 col-md-9">
-                                <h4>Updated Techonlogy</h4>
-                                <p>Neque porro quisquam est, qui do lorem ipsum quia dolor sit ametig, consectetur adipis civelit sed.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Mission subsection -->
-
-                    <!-- Mission subsection -->
-                    <div class="mission_sub_section col-12 col-md-6">
-                        <div class="row">
-                            <div class="icon col-3 col-sm-2 col-md-3">
-                                <img src="about/files/images/award_icon-1.png" alt="">
-                            </div>
-                            <div class="detail col-9 col-sm-10 col-md-9">
-                                <h4>Quality Assurance</h4>
-                                <p>Neque porro quisquam est, qui do lorem ipsum quia dolor sit ametig, consectetur adipis civelit sed.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Mission subsection -->
-
-                </div>
-            </div>
-            <!-- awards_rhs end -->
-        </div>
-    </div>
+    <?php include('about/awards.php'); ?>
     <!-- Awards section -->
 
     <!-- Our Team -->
