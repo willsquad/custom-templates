37774385edb96f7677ef0a3fa923e14791932a40
Ashil John <willsquad.aj@gmail.com>
2018-07-27 15:41:30 +0530

    News list Page added

---
+++ template-1/news/news_content.php
@@ -0,0 +1,110 @@
+<style>
+    <?php include('files/css/news.css'); ?>
+</style>
+
+<div id="news" class="col-12">
+    <div class="row max_width">
+        <!-- News LHS -->
+        <?php include('news_content_lhs.php') ?>
+        <!-- News LHS End -->
+
+        <!-- News RHS -->
+        <div class="news_rhs col-12 col-md-8 col-lg-9">
+
+            <!-- News Story -->
+            <div class="news_story">
+                <div class="news_image">
+                    <img src="news/files/images/news_img1.jpg" alt="">
+                    <div class="overlay">
+                        <div class="news_date">July 27, 2018</div>
+                    </div>
+                </div>
+
+                <div class="news_details d-flex flex-wrap">
+                    <div class="detail d-flex"><i class="material-icons">person</i> By: Admin</div>
+                    <div class="detail d-flex"><i class="material-icons">label</i> Industry/Manufacturing/Metal</div>
+                    <div class="detail d-flex"><i class="material-icons">chat</i> 3 Comments</div>
+                </div>
+
+                <div class="text">
+                    <h4><a href="#"></a> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, suscipit.</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos molestiae minus ullam error, natus saepe in iusto adipisci molestias accusamus asperiores est animi quidem consequuntur, expedita vel similique a culpa eaque laboriosam. Dolorem quae necessitatibus distinctio id. Deserunt nisi optio laboriosam sunt ad, sequi atque ex perspiciatis veniam quam totam...</p>
+                    <a href="#" class="read_more_button">Read More</a>
+                </div>
+            </div>
+            <!-- News Story -->
+
+            <!-- News Story -->
+            <div class="news_story">
+                <div class="news_image">
+                    <img src="news/files/images/news_img2.jpg" alt="">
+                    <div class="overlay">
+                        <div class="news_date">July 27, 2018</div>
+                    </div>
+                </div>
+
+                <div class="news_details d-flex flex-wrap">
+                    <div class="detail d-flex"><i class="material-icons">person</i> By: Admin</div>
+                    <div class="detail d-flex"><i class="material-icons">label</i> Industry/Manufacturing/Metal</div>
+                    <div class="detail d-flex"><i class="material-icons">chat</i> 3 Comments</div>
+                </div>
+
+                <div class="text">
+                    <h4><a href="#"></a> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, suscipit.</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos molestiae minus ullam error, natus saepe in iusto adipisci molestias accusamus asperiores est animi quidem consequuntur, expedita vel similique a culpa eaque laboriosam. Dolorem quae necessitatibus distinctio id. Deserunt nisi optio laboriosam sunt ad, sequi atque ex perspiciatis veniam quam totam...</p>
+                    <a href="#" class="read_more_button">Read More</a>
+                </div>
+            </div>
+            <!-- News Story -->
+
+            <!-- News Story -->
+            <div class="news_story">
+                <div class="news_image">
+                    <img src="news/files/images/news_img3.jpg" alt="">
+                    <div class="overlay">
+                        <div class="news_date">July 27, 2018</div>
+                    </div>
+                </div>
+
+                <div class="news_details d-flex flex-wrap">
+                    <div class="detail d-flex"><i class="material-icons">person</i> By: Admin</div>
+                    <div class="detail d-flex"><i class="material-icons">label</i> Industry/Manufacturing/Metal</div>
+                    <div class="detail d-flex"><i class="material-icons">chat</i> 3 Comments</div>
+                </div>
+
+                <div class="text">
+                    <h4><a href="#"></a> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, suscipit.</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos molestiae minus ullam error, natus saepe in iusto adipisci molestias accusamus asperiores est animi quidem consequuntur, expedita vel similique a culpa eaque laboriosam. Dolorem quae necessitatibus distinctio id. Deserunt nisi optio laboriosam sunt ad, sequi atque ex perspiciatis veniam quam totam...</p>
+                    <a href="#" class="read_more_button">Read More</a>
+                </div>
+            </div>
+            <!-- News Story -->
+
+            <!-- News Story -->
+            <div class="news_story">
+                <div class="news_image">
+                    <img src="news/files/images/news_img4.jpg" alt="">
+                    <div class="overlay">
+                        <div class="news_date">July 27, 2018</div>
+                    </div>
+                </div>
+
+                <div class="news_details d-flex flex-wrap">
+                    <div class="detail d-flex"><i class="material-icons">person</i> By: Admin</div>
+                    <div class="detail d-flex"><i class="material-icons">label</i> Industry/Manufacturing/Metal</div>
+                    <div class="detail d-flex"><i class="material-icons">chat</i> 3 Comments</div>
+                </div>
+
+                <div class="text">
+                    <h4><a href="#"></a> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, suscipit.</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos molestiae minus ullam error, natus saepe in iusto adipisci molestias accusamus asperiores est animi quidem consequuntur, expedita vel similique a culpa eaque laboriosam. Dolorem quae necessitatibus distinctio id. Deserunt nisi optio laboriosam sunt ad, sequi atque ex perspiciatis veniam quam totam...</p>
+                    <a href="#" class="read_more_button">Read More</a>
+                </div>
+            </div>
+            <!-- News Story -->
+
+
+        </div>
+        <!-- News RHS End -->
+    </div>
+</div>
