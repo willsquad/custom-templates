daad8d2048b89d425fb7b091753004c86d500880
Ashil John <willsquad.aj@gmail.com>
2018-07-27 11:42:36 +0530

    Projects page hover effects added

--- template-1/projects/projects_content.php
+++ template-1/projects/projects_content.php
@@ -17,31 +17,112 @@
     
     <div class="projects_grid grid row">
             <div class="element-item automotive col-12 col-lg-4" data-category="automotive">
-                <img src="projects/files/images/project_img1.jpg" alt="">
+                <div class="image_and_overlay_container">
+                    <img src="projects/files/images/project_img1.jpg" alt="">
+
+                    <div class="overlay">
+                        <a class="view_project" href="#">View Project</a>
+                        <div class="project_name">
+                            Project Name #1
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="element-item chemical col-12 col-lg-4" data-category="chemical">
-                <img src="projects/files/images/project_img2.jpg" alt="">
+                <div class="image_and_overlay_container">
+                    <img src="projects/files/images/project_img2.jpg" alt="">
+
+                    <div class="overlay">
+                        <a class="view_project" href="#">View Project</a>
+                        <div class="project_name">
+                            Project Name #2
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="element-item construction col-12 col-lg-4" data-category="construction">
-                <img src="projects/files/images/project_img3.jpg" alt="">
+                <div class="image_and_overlay_container">
+                    <img src="projects/files/images/project_img3.jpg" alt="">
+
+                    <div class="overlay">
+                        <a class="view_project" href="#">View Project</a>
+                        <div class="project_name">
+                            Project Name #3
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="element-item energy col-12 col-lg-4" data-category="energy">
-                <img src="projects/files/images/project_img4.jpg" alt="">
+                <div class="image_and_overlay_container">
+                    <img src="projects/files/images/project_img4.jpg" alt="">
+
+                    <div class="overlay">
+                        <a class="view_project" href="#">View Project</a>
+                        <div class="project_name">
+                            Project Name #4
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="element-item exploration col-12 col-lg-4" data-category="exploration">
-                <img src="projects/files/images/project_img5.jpg" alt="">
+                <div class="image_and_overlay_container">
+                    <img src="projects/files/images/project_img5.jpg" alt="">
+
+                    <div class="overlay">
+                        <a class="view_project" href="#">View Project</a>
+                        <div class="project_name">
+                            Project Name #5
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="element-item automotive col-12 col-lg-4" data-category="automotive">
-                <img src="projects/files/images/project_img6.jpg" alt="">
+                <div class="image_and_overlay_container">
+                    <img src="projects/files/images/project_img6.jpg" alt="">
+
+                    <div class="overlay">
+                        <a class="view_project" href="#">View Project</a>
+                        <div class="project_name">
+                            Project Name #6
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="element-item chemical col-12 col-lg-4" data-category="chemical">
-                <img src="projects/files/images/project_img7.jpg" alt="">
+                <div class="image_and_overlay_container">
+                    <img src="projects/files/images/project_img9.jpg" alt="">
+
+                    <div class="overlay">
+                        <a class="view_project" href="#">View Project</a>
+                        <div class="project_name">
+                            Project Name #7
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="element-item construction col-12 col-lg-4" data-category="construction">
-                <img src="projects/files/images/project_img8.jpg" alt="">
+                <div class="image_and_overlay_container">
+                    <img src="projects/files/images/project_img8.jpg" alt="">
+
+                    <div class="overlay">
+                        <a class="view_project" href="#">View Project</a>
+                        <div class="project_name">
+                            Project Name #8
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="element-item energy col-12 col-lg-4" data-category="energy">
-                <img src="projects/files/images/project_img9.jpg" alt="">
+                <div class="image_and_overlay_container">
+                    <img src="projects/files/images/project_img9.jpg" alt="">
+
+                    <div class="overlay">
+                        <a class="view_project" href="#">View Project</a>
+                        <div class="project_name">
+                            Project Name #7
+                        </div>
+                    </div>
+                </div>
             </div>
     </div>
 
