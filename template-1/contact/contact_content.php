446cfdbb9be860691d1661b1c14648f5ef47bbe6
Ashil John <willsquad.aj@gmail.com>
2018-07-28 15:40:38 +0530

    Contact Us page in progress

---
+++ template-1/contact/contact_content.php
@@ -0,0 +1,38 @@
+<style>
+    <?php include('files/css/contact.css'); ?>
+</style>
+
+<div id="contact" class="col-12">
+    <div class="row max_width">
+        <div class="col-12">
+            <h2 class="section_title">Get in Touch</h2>
+
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas ducimus architecto possimus, ullam sunt magni eligendi fuga error sequi dolor sit aperiam culpa, officiis minus libero delectus consequuntur nemo quasi!</p>
+
+        </div>
+        <div class="col-12">
+            <div class="row">
+                <div class="contact_form_lhs col-12 col-md-8">
+                    <div class="contact_input">
+                        <input type="text" placeholder="Name">
+                    </div>
+                    <div class="contact_input">
+                        <input type="text" placeholder="Email">
+                    </div>
+                    <div class="contact_input">
+                        <input type="text" placeholder="Subject">
+                    </div>
+                    <div class="contact_input">
+                        <textarea name="" placeholder="Special Request"></textarea>
+                    </div>
+
+                    <div class="submit_button ">
+                        <button>Submit Now</button>
+                    </div>
+                </div>
+                <div class="contact_details_rhs col-12 col-md-4"></div>
+            </div>
+        </div>
+
+    </div>
+</div>
