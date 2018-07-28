d2c0bba84fc5f06af06d260ff27b695d41910308
Ashil John <willsquad.aj@gmail.com>
2018-07-28 09:05:51 +0530

    News content split

---
+++ template-1/news/news_content_rhs.php
@@ -0,0 +1,16 @@
+<div class="news_rhs col-12 col-md-8 col-lg-9">
+
+    <!-- News Story -->
+    <?php include('news_story_div.php') ?>
+    <!-- News Story -->
+    <!-- News Story -->
+    <?php include('news_story_div.php') ?>
+    <!-- News Story -->
+    <!-- News Story -->
+    <?php include('news_story_div.php') ?>
+    <!-- News Story -->
+    <!-- News Story -->
+    <?php include('news_story_div.php') ?>
+    <!-- News Story -->
+
+</div>
