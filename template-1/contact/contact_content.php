032ba6a58994acc8d3d937ed57a9d963117d4aff
Ashil John <willsquad.aj@gmail.com>
2018-07-31 12:15:43 +0530

    Contact Us page split

--- template-1/contact/contact_content.php
+++ template-1/contact/contact_content.php
@@ -13,52 +13,12 @@
         <div class="col-12">
             <div class="row">
 
-                 <!-- Contact Details LHS -->
-                <div class="contact_form_lhs col-12 col-lg-8">
-                    <div class="contact_input">
-                        <input type="text" placeholder="Name">
-                    </div>
-                    <div class="contact_input">
-                        <input type="text" placeholder="Email">
-                    </div>
-                    <div class="contact_input">
-                        <input type="text" placeholder="Subject">
-                    </div>
-                    <div class="contact_input">
-                        <textarea name="" placeholder="Special Request"></textarea>
-                    </div>
-
-                    <div class="submit_button ">
-                        <button>Submit Now</button>
-                    </div>
-                </div>
-                 <!-- Contact Details LHS -->
+                 <!-- Contact Form LHS -->
+                <?php include('contact_form.php');?>
+                 <!-- Contact Form LHS -->
 
                 <!-- Contact Details RHS -->
-                <div class="contact_details_rhs col-12 col-lg-4">
-                    <div class="contact_details_container">
-
-                        <!-- Contact RHS Detail -->
-                        <div class="contact_detail">
-                            <div class="icon"><i class="material-icons">location_on</i></div>
-                            <div class="detail"><p class="">Building, Locality<br>Street, State, 12345</p></div>
-                        </div>
-                        <!-- Contact RHS Detail -->
-                        <!-- Contact RHS Detail -->
-                        <div class="contact_detail">
-                            <div class="icon"><i class="material-icons">mail_outline</i></div>
-                            <div class="detail"><p class="">info@company.com<br>support@company.com</p></div>
-                        </div>
-                        <!-- Contact RHS Detail -->
-                        <!-- Contact RHS Detail -->
-                        <div class="contact_detail">
-                            <div class="icon"><i class="material-icons">phone</i></div>
-                            <div class="detail"><p class="">+ 1800 123 4567<br>+ 1800 234 5678</p></div>
-                        </div>
-                        <!-- Contact RHS Detail -->
-
-                    </div>
-                </div>
+                <?php include('contact_details_rhs.php');?>
                  <!-- Contact Details RHS -->
 
             </div>
@@ -67,6 +27,7 @@
     </div>
 </div>
 
-<div class="map_full_width_row">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2381.7399040776495!2d-6.261147484122739!3d53.34791197997939!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1&#9;&#9;&#9;!3m3!3m2!1sen!2sus!4v1462581622087" width="600" height="450" frameborder="0" style="border:0" allowfullscreen=""></iframe>
-</div>
+
+<!-- Contact Map -->
+<?php include('contact_map.php');?>
+<!-- Contact Map -->
