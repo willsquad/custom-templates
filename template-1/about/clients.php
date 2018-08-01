259f1489c9d1cd5da1970ef0f30dd9ce0f2d6c9f
Ashil John <willsquad.aj@gmail.com>
2018-08-01 13:56:20 +0530

    Template 2 started. Clients slider modified

--- template-1/about/clients.php
+++ template-1/about/clients.php
@@ -2,7 +2,7 @@
     <?php include('files/css/clients.css'); ?>
 </style>
 
-<div id="clients_slider">
+<div id="clients_slider" class="col-12">
     <div class="max_width">
         <div class="slider_container clients_slide_container">
             <div class="client"><img src="about/files/images/client_img1.jpg" alt=""></div>
