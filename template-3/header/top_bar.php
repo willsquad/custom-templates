e2b787318b971f88347478309493e0e2140fee0b
Ashil John <willsquad.aj@gmail.com>
2018-08-06 14:06:46 +0530

    Template 3 : Navigation added

--- template-3/header/top_bar.php
+++ template-3/header/top_bar.php
@@ -4,10 +4,16 @@
 
 <div class="top_bar col-12 d-flex align-items-center hidden-sm-down">
     <div class="top_bar__content max_width d-flex justify-content-between">
-        <div class="top_bar__content__lhs d-flex align-items-center"><i class="material-icons">check</i> Certified ISO 9001:2009</div>
-        <div class="top_bar__content__rhs d-flex align-items-center">
+        <div class="top_bar__content__lhs d-flex align-items-center">
             <span class="d-flex align-items-center"><i class="material-icons">phone</i> (1800) 456 7890</span>
             <span class="d-flex align-items-center"><i class="material-icons">mail_outline</i> info@customtheme.com</span>
+        </div>
+        <div class="top_bar__content__rhs d-flex align-items-center">
+            <span class="d-flex align-items-center"><i class="far fa-envelope"></i></span>
+            <span class="d-flex align-items-center"><i class="fab fa-facebook-f"></i></span>
+            <span class="d-flex align-items-center"><i class="fab fa-twitter"></i></span>
+            <span class="d-flex align-items-center"><i class="fab fa-google-plus-g"></i></span>
+            <span class="d-flex align-items-center"><i class="fab fa-linkedin"></i></span>
         </div>
     </div>
 </div>
