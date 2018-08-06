150da14264e2e16391f98d339c1282a4273be1d3
Ashil John <willsquad.aj@gmail.com>
2018-08-06 09:22:11 +0530

    Template 3 : Index page : Latest News modified

--- template-3/latest_news/latest_news.php
+++ template-3/latest_news/latest_news.php
@@ -4,39 +4,23 @@
 
 <div id="latest_news" class="col-12">
     <div class="row max_width">
-        <h2 class="col-12 section_title">Latest News</h2>
+        <h2 class="section_title">Latest News</h2>
 
-        <div class="col-12 news_lhs">
-            
+        <div class="row">
+
             <!-- News Div -->
-            <div class="row news_div">
-                <div class="image_div col-lg-6 col-md-6 col-12">
-                    <img src="latest_news/files/images/news_img1.jpg" alt="">
-                </div>
-                <div class="content_div col-lg-6 col-md-6 col-12">
-                    <a href="news-details.php" class="title"><h5>Lorem ipsum dolor sit, amet consectetur adipisicing</h5></a>
-
-                    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta necessitatibus ducimus tempora...</p>
-
-                    <a href="news-details.php" class="read_more">Read More</a>
-                </div>
-            </div>
+            <?php include('news_div.php'); ?>
             <!-- News Div -->
 
             <!-- News Div -->
-            <div class="row news_div">
-                <div class="image_div col-lg-6 col-md-6 col-12">
-                    <img src="latest_news/files/images/news_img2.jpg" alt="">
-                </div>
-                <div class="content_div col-lg-6 col-md-6 col-12">
-                    <a href="news-details.php" class="title"><h5>Lorem ipsum dolor sit, amet consectetur adipisicing</h5></a>
-
-                    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta necessitatibus ducimus tempora...</p>
-
-                    <a href="news-details.php" class="read_more">Read More</a>
-                </div>
-            </div>
+            <?php include('news_div.php'); ?>
             <!-- News Div -->
+
+            <!-- News Div -->
+            <?php include('news_div.php'); ?>
+            <!-- News Div -->
+
+
         </div>
     </div>
 </div>
