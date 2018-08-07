d84e34ffd757ebc884a4a16f362c4aa3726a1742
Ashil John <willsquad.aj@gmail.com>
2018-08-07 13:11:56 +0530

    Template 3 : Index page : Our services section added.

--- template-3/index_our_solutions/index_our_solutions.php
+++ template-3/index_our_solutions/index_our_solutions.php
@@ -4,6 +4,72 @@
 
 <div id="index_our_solutions" class="col-12">
     <div class="row max_width">
-        
+
+        <!-- Our services heading -->
+        <div class="heading_container col-12">
+            <div class="row">
+                <div class="col-12 col-lg-3"><h3 class="heading_h3">Our Services</h3></div>
+                <div class="col-12 col-lg-9"><p class="heading_p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p></div>
+            </div>
+        </div>
+        <!-- Our services heading -->
+
+        <!-- Services divs container -->
+        <div class="services_divs_container col-12">
+            <div class="row">
+
+                <!-- Services div -->
+                <div class="col-12 col-md-6 col-lg-4 services_div">
+                    <div class="img service_img"></div>
+                    <div class="heading"><h4><a href="#">Service</a></h4></div>
+                    <div class="paragraph"><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, facere soluta? Voluptatem iste animi placeat.</p></div>
+                    <div class="read_more"><a href="#">Read More</a></div>
+                </div>
+                <!-- Services div -->
+                <!-- Services div -->
+                <div class="col-12 col-md-6 col-lg-4 services_div">
+                    <div class="img service_img"></div>
+                    <div class="heading"><h4><a href="#">Service</a></h4></div>
+                    <div class="paragraph"><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, facere soluta? Voluptatem iste animi placeat.</p></div>
+                    <div class="read_more"><a href="#">Read More</a></div>
+                </div>
+                <!-- Services div -->
+                <!-- Services div -->
+                <div class="col-12 col-md-6 col-lg-4 services_div">
+                    <div class="img service_img"></div>
+                    <div class="heading"><h4><a href="#">Service</a></h4></div>
+                    <div class="paragraph"><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, facere soluta? Voluptatem iste animi placeat.</p></div>
+                    <div class="read_more"><a href="#">Read More</a></div>
+                </div>
+                <!-- Services div -->
+                <!-- Services div -->
+                <div class="col-12 col-md-6 col-lg-4 services_div">
+                    <div class="img service_img"></div>
+                    <div class="heading"><h4><a href="#">Service</a></h4></div>
+                    <div class="paragraph"><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, facere soluta? Voluptatem iste animi placeat.</p></div>
+                    <div class="read_more"><a href="#">Read More</a></div>
+                </div>
+                <!-- Services div -->
+                <!-- Services div -->
+                <div class="col-12 col-md-6 col-lg-4 services_div">
+                    <div class="img service_img"></div>
+                    <div class="heading"><h4><a href="#">Service</a></h4></div>
+                    <div class="paragraph"><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, facere soluta? Voluptatem iste animi placeat.</p></div>
+                    <div class="read_more"><a href="#">Read More</a></div>
+                </div>
+                <!-- Services div -->
+                <!-- Services div -->
+                <div class="col-12 col-md-6 col-lg-4 services_div">
+                    <div class="img service_img"></div>
+                    <div class="heading"><h4><a href="#">Service</a></h4></div>
+                    <div class="paragraph"><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, facere soluta? Voluptatem iste animi placeat.</p></div>
+                    <div class="read_more"><a href="#">Read More</a></div>
+                </div>
+                <!-- Services div -->
+
+            </div>
+        </div>
+        <!-- Services divs container -->
+
     </div>
 </div>
---
+++ template-3/index_our_solutions/index_service_div.php
@@ -0,0 +1,10 @@
+<style>
+    <?php include('files/css/index_service_div.css'); ?>
+</style>
+
+<div class="col-12 col-md-6 col-lg-4 services_div">
+    <div class="img service_img"></div>
+    <div class="heading"><h4><a href="#">Service</a></h4></div>
+    <div class="paragraph"><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, facere soluta? Voluptatem iste animi placeat.</p></div>
+    <div class="read_more"><a href="#">Read More</a></div>
+</div>
