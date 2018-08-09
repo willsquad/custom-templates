83296f4bb11b7912305bd14fae3062b002b46755
Ashil John <willsquad.aj@gmail.com>
2018-08-09 15:35:20 +0530

    Template 5 : All pages added

---
+++ template-5/contact/contact_content.php
@@ -0,0 +1,33 @@
+<style>
+    <?php include('files/css/contact.css'); ?>
+</style>
+
+<div id="contact" class="col-12">
+    <div class="row max_width">
+        <div class="col-12">
+            <h2 class="section_title">Get in Touch</h2>
+
+            <p class="contact_p">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas ducimus architecto possimus, ullam sunt magni eligendi fuga error sequi dolor sit aperiam culpa, officiis minus libero delectus consequuntur nemo quasi!</p>
+
+        </div>
+        <div class="col-12">
+            <div class="row">
+
+                 <!-- Contact Form LHS -->
+                <?php include('contact_form.php');?>
+                 <!-- Contact Form LHS -->
+
+                <!-- Contact Details RHS -->
+                <?php include('contact_details_rhs.php');?>
+                 <!-- Contact Details RHS -->
+
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+<!-- Contact Map -->
+<?php include('contact_map.php');?>
+<!-- Contact Map -->
---
+++ template-5/news/news_content.php
@@ -0,0 +1,28 @@
+<style>
+    <?php include('files/css/news.css'); ?>
+</style>
+
+<div id="news" class="col-12">
+    <div class="row max_width">
+        <!-- News LHS -->
+        <?php include('news_content_lhs.php') ?>
+        <!-- News LHS End -->
+
+        <!-- News RHS -->
+        <div class="news_rhs col-12 col-md-8 col-lg-9">
+            <!-- News Story -->
+            <?php include('news_story_div.php') ?>
+            <!-- News Story -->
+            <!-- News Story -->
+            <?php include('news_story_div.php') ?>
+            <!-- News Story -->
+            <!-- News Story -->
+            <?php include('news_story_div.php') ?>
+            <!-- News Story -->
+            <!-- News Story -->
+            <?php include('news_story_div.php') ?>
+            <!-- News Story -->
+        </div>
+        <!-- News RHS End -->
+    </div>
+</div>
