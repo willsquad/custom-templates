8b6a7c3e894c61cae69c7d7346a0fc4c9a4bb3a7
Ashil John <willsquad.aj@gmail.com>
2018-08-14 14:45:16 +0530

    Template 5 JS Modified

--- template-5/files/js/main.js
+++ template-5/files/js/main.js
@@ -41,8 +41,144 @@ $(document).ready(function(){
                 }
             ]
         });
-      } 
-       else if ($(this).is(".clients_slide_container")){
+      } else if ($(this).is(".projects_gallery_slider_container")) {
+        $(this).slick({
+          prevArrow: false,
+          nextArrow: false,
+          dots:false,
+          initialSlide: 0,
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          swipeToSlide: true,
+          centerMode: false,
+          centerPadding: '20px',
+          autoplay: true,
+          autoplaySpeed: 2000,
+          responsive: [
+              {
+              breakpoint: 1400,
+              settings: {
+                  slidesToShow:3,
+                  slidesToScroll: 1,
+                  infinite: true
+              }
+              },
+              {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow:2,
+                    slidesToScroll: 1,
+                    infinite: true
+                }
+              },
+              {
+              breakpoint: 1000,
+              settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1
+              }
+              },
+              {
+              breakpoint: 850,
+              settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+              }
+              }
+          ]
+      });
+    } else if ($(this).is(".what_we_offer_slider")) {
+        $(this).slick({
+          prevArrow: $('.prev-slide-offer'),
+          nextArrow: $('.next-slide-offer'),
+          dots:false,
+          initialSlide: 0,
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          swipeToSlide: true,
+          centerMode: false,
+          centerPadding: '20px',
+          autoplay: true,
+          autoplaySpeed: 2000,
+          responsive: [
+              {
+              breakpoint: 1200,
+              settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 1,
+                  infinite: true
+              }
+              },
+              {
+              breakpoint: 767,
+              settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1
+              }
+              },
+              {
+              breakpoint: 600,
+              settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+              }
+              }
+          ]
+      });
+    } else if ($(this).is(".team_slider_container")){
+        $(this).slick({
+            prevArrow: $('.prev-slide-team'),
+            nextArrow: $('.next-slide-team'),
+            dots:false,
+            initialSlide: 0,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            swipeToSlide: true,
+            centerMode: false,
+            centerPadding: '20px',
+            autoplay: true,
+            autoplaySpeed: 2000,
+            responsive: [
+                {
+                    breakpoint: 1600,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                        infinite: true
+                    }
+                },
+                {
+                breakpoint: 1100,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true
+                }
+                },
+                {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+                },
+                {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+                },
+                {
+                breakpoint: 450,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+                }
+            ]
+        });
+      } else if ($(this).is(".clients_slide_container")){
         $(this).slick({
             dots:false,
             prevArrow: false,
@@ -103,7 +239,6 @@ $(document).ready(function(){
             initialSlide: 0,
             slidesToShow: 1,
             slidesToScroll: 1,
-            swipeToSlide: true,
             centerMode: false,
             centerPadding: '20px',
             autoplay: true,
